6f50c2a16755587e1d75eb7ed4b57fdec4f72d59
jasonhilimire <jasonhilimire@mac.com>
2020-01-08 09:02:23 -0700

    Initial Commit

---
+++ CoreDataRelationship/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CoreDataRelationship
+//
+//  Created by Jason Hilimire on 1/8/20.
+//  Copyright © 2020 Jason Hilimire. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
